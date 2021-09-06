ddf5924ffd7d5bed21734ff5d3163e22abb9652a
tessema2021 <ninafuchere@gmail.com>
2021-09-05 19:45:02 -0500

    footer added

---
+++ src/scripts/footer/footer.js
@@ -0,0 +1,25 @@
+export const footbar = () => {
+    return ` <footer class="footer_nav">
+        <div class="footer_image">
+            <img src="./images/foot.png" alt="footer image" />
+        </div>
+        <div class="navigation__item navigation__search">
+            <input type="text" id="footer_postSearch" placeholder="Search posts..." />
+        </div>
+        <div class="navigation__item navigation__logout">
+            <button id="footer_logout" class="fakeLink">Logout</button>
+        </div>
+        <div class="footer__item">
+                Posts since <select id="yearSelection">
+                    <option>2020</option>
+                    <option>2019</option>
+                    <option>2018</option>
+                    <option>2017</option>
+                </select>
+                <span id="postCount">0</span>
+            </div>
+    </footer>`
+}
+
+
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,14 @@ import { getUsers, getPosts } from "./data/DataManager.js";
 import { NavBar } from "./nav/NavBar.js";
 
 import { PostList } from "./feed/postlist.js";
+import { footbar } from "./footer/footer.js";
 
-// const allUsers = getUsers()
-//     .then(apiUsers => {
-//         console.log("allUsers", apiUsers)
-//     }
+const allUsers = getUsers()
+    .then(apiUsers => {
+        console.log("allUsers", apiUsers)
+    }
 
-//     )
+    )
 
 
 
@@ -37,20 +38,49 @@ const showNavBar = () => {
 }
 
 
+const showFootBar = () => {
+    const footerElement = document.querySelector(".footer_nav")
+    footerElement.innerHTML = footbar();
+}
+
+
 
 
 
 const applicationElement = document.querySelector(".giffygram");
 
-applicationElement.addEventListener("click", event => {
+// applicationElement.addEventListener("click", event => {
+//     console.log("what was clicked", event.target)
+//     if (event.target.id === "logout") {
+//         console.log("are you sure you want logout?")
+//     }
+// })
+// the same fucntion in different way
+const giffyclick = (event) => {
+    console.log("what was clicked", event.target)
     if (event.target.id === "logout") {
-        //console.log("You clicked on logout")
+        console.log("are you sure you want logout?")
     }
-})
+}
+applicationElement.addEventListener("click", giffyclick)
+
+
+
 
 const startGiffyGram = () => {
     showPostList();
     showNavBar();
+    showFootBar();
 }
 
 startGiffyGram();
+
+
+
+applicationElement.addEventListener("change", event => {
+    if (event.target.id === "yearSelection") {
+        const yearAsNumber = parseInt(event.target.value)
+
+        console.log(`User wants to see posts since ${yearAsNumber}`)
+    }
+})
